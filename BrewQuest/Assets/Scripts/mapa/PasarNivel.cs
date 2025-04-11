a515fcb52237b882e23bea77a03e6179e3f3bb1a
Carlos Blasco <carlos.blasco@enti.cat>
2025-04-11 11:18:14 +0200

    Mejoro Entrada de puerta y textos
    ahora tienes que darle a w para entra y los textos son mejores

--- BrewQuest/Assets/Scripts/mapa/PasarNivel.cs
+++ BrewQuest/Assets/Scripts/mapa/PasarNivel.cs
@@ -1,15 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 public class PasarNivel : MonoBehaviour
 {
-   private void OnTriggerEnter2D(Collider2D collision)
+    private bool jugadorEnZona = false;
+
+    private void Update()
     {
-        if(collision.CompareTag("Player"))
+        if (jugadorEnZona && Input.GetKeyDown(KeyCode.W))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+            else
+            {
+                Debug.Log("¡Último nivel alcanzado!");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            jugadorEnZona = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            jugadorEnZona = false;
         }
     }
 }
