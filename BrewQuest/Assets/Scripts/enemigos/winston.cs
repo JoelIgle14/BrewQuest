ba444cc273720cb64b175fc8eb02f1daa9e7c806
SrOscar <oscar.duque@enti.cat>
2025-04-27 21:10:47 +0200

    Nivel 1 medio hecho
    con pinchos, plataformas mobiles y con gravedad y con algun enemigo, con obtencion de dash y un enemigo con knockback

--- BrewQuest/Assets/Scripts/enemigos/winston.cs
+++ BrewQuest/Assets/Scripts/enemigos/winston.cs
@@ -8,21 +8,27 @@ public class winston : MonoBehaviour
     public float speed = 2f;
 
     //logica de golpe
-    public float golpeForce = 3f; // fuerza del empujón
-    public float golpeDuration = 2f; // cuánto dura el empujón
+    public float golpeForceX = 3f;
+    public float golpeForceY = 3f;
+    public float golpeDuration = 0.5f; 
 
     private bool isBeingHit = false; // Para controlar si está siendo golpeado
 
     private Vector3 startPosition;
     private int direction = 1;
 
-    //otros scripts
+    //otros scripts o componentes
     private Enemyvida ev;
+    private Rigidbody2D rb;
+    private PlayerMovement jarry;
 
     void Start()
     {
         startPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
         ev = GetComponent<Enemyvida>();
+        jarry = FindObjectOfType<PlayerMovement>();
+
     }
 
     void Update()
@@ -53,19 +59,21 @@ public class winston : MonoBehaviour
 
         IEnumerator GolpeCoroutine()
         {
-            float timer = 0f;
+            //KnockBack aplicado desde el rigidbody
 
-            // Mientras dure el golpe, empujamos al enemigo
-            while (timer < golpeDuration)
-            {
-                transform.position += Vector3.left * golpeForce * Time.deltaTime;
-                timer += Time.deltaTime;
-                yield return null; // Espera un frame
-            }
+            float direction = (jarry.transform.position.x > transform.position.x) ? -1f : 1f;
 
-            // Después de que el golpe termine, reiniciamos las variables
-            isBeingHit = false; // El enemigo ya no está siendo golpeado
-            ev.golpeado = false; // Reiniciamos el estado de golpe
+            Vector2 baseDirection = new Vector2(direction, 1f).normalized; // con esta linea va igual de fuerte a los dos lados
+            Vector2 golpeVector = new Vector2(baseDirection.x * golpeForceX, baseDirection.y * golpeForceY); //esta es la del empuje
+
+            //reseteo y aplicacion de fuerza
+            rb.velocity = Vector2.zero;
+            rb.AddForce(golpeVector, ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(golpeDuration);
+   
+            isBeingHit = false;
+            ev.golpeado = false;
         }
     }
 }
