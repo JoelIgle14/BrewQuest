bcd17c6969c6a3916b670e7a5bb0cf4fbbaed58c
Joel Iglesias Sancho <joel.iglesias@enti.cat>
2025-04-30 10:55:15 +0200

    Subo pause menu, funcionan los 3 botones

---
+++ BrewQuest/Assets/Scripts/Game/PauseMenuController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuController : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void RestartLevel()
+    {
+        Time.timeScale = 1f; // Asegúrate de reanudar el tiempo
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
