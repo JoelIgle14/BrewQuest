b278bec8928061727aedffa794e65dc386024ae0
Joel Iglesias Sancho <joel.iglesias@enti.cat>
2025-05-07 14:13:02 +0200

    respawn si te mata enemy

---
+++ BrewQuest/Assets/Scripts/CheckpointData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class CheckpointData
+{
+    public static Vector3? ultimaPosicionCheckpoint = null;
+}
+
