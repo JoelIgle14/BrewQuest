20d2b6bdc4ec9947eb60a0cd1c4fd5e9e426f3eb
Paco Gerte <oscar.duque@enti.cat>
2025-05-12 11:26:37 +0200

    Invulnerabilidad y mejora ataque
    espabila jowi un poco que era lamentable COSA

--- BrewQuest/Assets/Scripts/jarry/playeratac.cs
+++ BrewQuest/Assets/Scripts/jarry/playeratac.cs
@@ -4,27 +4,21 @@ using UnityEngine;
 
 public class playeratac : MonoBehaviour
 {
-    public Vector2 attackBoxSize = new Vector2(1f, 1f);  // Tamaño del área de ataque
-
-
     public float attackRange;
     public float attackDamage;
     public float attacCooldown;
 
-    private float timeToNextAttack = 1.0f;
+    private float timeToNextAttack = 0f;
     public Vector3 positionAttack;
 
-
-    private bool lookingup;  // Esto es suficiente como un bool, ya que estamos determinando si est� mirando hacia arriba
+    private bool lookingup;
     private Enemyvida ev;
-    Animator animator;
+    private Animator animator;
 
     void Start()
     {
         ev = GetComponent<Enemyvida>();
         animator = GetComponent<Animator>();
-        Debug.Log(animator);
-
     }
 
     void Update()
@@ -32,29 +26,25 @@ public class playeratac : MonoBehaviour
         LookingUp();
         CalculateAttackPosition();
 
-        if (lookingup)
-        {
-            positionAttack = transform.position + new Vector3(0f, 0.7f, 0f);
-        }
-        else
-        {
-            positionAttack = transform.position + new Vector3(0.6f * Mathf.Sign(transform.localScale.x), 0f, 0f);
-        }
-
         if (Time.time >= timeToNextAttack)
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
+                // Registrar el tiempo para el siguiente ataque
                 timeToNextAttack = Time.time + attacCooldown;
+
+                // Lanzar animación
                 animator.SetTrigger("ataque");
+
+                // Aplicar el daño inmediatamente
+                DealDamage();
             }
         }
-
     }
 
-    public void DealDamage()
+    private void DealDamage()
     {
-        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(positionAttack, attackBoxSize, 0f);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(positionAttack, attackRange);
 
         foreach (Collider2D enemy in hitEnemies)
         {
@@ -64,7 +54,7 @@ public class playeratac : MonoBehaviour
                 if (enemyvid != null)
                 {
                     enemyvid.TakeDamage(attackDamage, gameObject, true);
-                    Debug.Log("golpe a enemigo");
+                    Debug.Log("Enemigo golpeado inmediatamente");
                 }
             }
         }
@@ -76,31 +66,20 @@ public class playeratac : MonoBehaviour
         Gizmos.DrawWireSphere(positionAttack, attackRange);
     }
 
-
-    void LookingUp()
+    private void LookingUp()
     {
         lookingup = Input.GetKey(KeyCode.UpArrow);
     }
 
-    void CalculateAttackPosition()
+    private void CalculateAttackPosition()
     {
-        if (Application.isPlaying)
+        if (lookingup)
         {
-            if (lookingup)
-            {
-                positionAttack = transform.position + new Vector3(0f, 0.7f, 0f);
-            }
-            else
-            {
-                positionAttack = transform.position + new Vector3(0.6f * Mathf.Sign(transform.localScale.x), 0f, 0f);
-            }
+            positionAttack = transform.position + new Vector3(0f, 0.7f, 0f);
         }
         else
         {
-            // Asume mirando hacia la derecha y no hacia arriba cuando estás en modo editor
-            positionAttack = transform.position + new Vector3(0.6f, 0f, 0f);
+            positionAttack = transform.position + new Vector3(0.6f * Mathf.Sign(transform.localScale.x), 0f, 0f);
         }
     }
-
 }
-
