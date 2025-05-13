f77d81f330ff579fb9678fa54fee720ad0595151
Oscar Duque <oscar.duque@enti.cat>
2025-05-13 12:51:55 +0200

    fuente animada
    en proceso

---
+++ BrewQuest/Assets/Scripts/elementos/runfuente.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class runfuente : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) 
+        {
+            animator.SetTrigger("activa");
+        }
+    }
+}
