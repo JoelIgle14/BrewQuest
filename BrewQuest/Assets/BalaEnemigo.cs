18dde892b9e56ec9bd238ae5c3f183914224c076
Enrique Giner <enrique.giner@enti.cat>
2025-05-21 13:02:21 +0200

    Jack
    Sin acabar

---
+++ BrewQuest/Assets/BalaEnemigo.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BalaEnemigo : MonoBehaviour
+{
+    public float velocidad = 5f;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * velocidad * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Aquí puedes aplicar daño al jugador
+            Destroy(gameObject);
+        }
+        else if (!other.CompareTag("Enemigo") && !other.CompareTag("BalaEnemigo"))
+        {
+            Destroy(gameObject); // Destruye al tocar otra cosa
+        }
+    }
+}
