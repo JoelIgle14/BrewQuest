95d1bf3eacaf90052e2936334f95780f73eb1889
enric-1003 <enrique.giner@enti.cat>
2025-05-31 12:09:46 +0200

    Musica nivel 1

--- BrewQuest/Assets/Scripts/mapa/Respawn.cs
+++ BrewQuest/Assets/Scripts/mapa/Respawn.cs
@@ -58,7 +58,7 @@ public class PlayerController : MonoBehaviour
         if (collision.CompareTag("Checkpoint"))
         {
 
-            controller.SeleccionAudio(1, 0.5f);
+            controller.SeleccionAudio(1, 0.2f);
             // Sumamos puntos si el checkpoint no ha sido activado antes
             Checkpoint checkpoint = collision.GetComponent<Checkpoint>();
             if (checkpoint != null && !checkpoint.yaActivado)
