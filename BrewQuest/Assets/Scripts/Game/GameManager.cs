4264c6d22065eea02232bb6bfa67dccd36749245
SrOscar <oscar.duque@enti.cat>
2025-06-02 17:27:06 +0200

    Ahora cuando hablas con Jarry, no se oyen los pasos ni mierdas asi

--- BrewQuest/Assets/Scripts/Game/GameManager.cs
+++ BrewQuest/Assets/Scripts/Game/GameManager.cs
@@ -17,7 +17,6 @@ public class GameManager : MonoBehaviour
     public bool hasDoubleJump = false;
     public bool hasShoot = false;
 
-
     public HUD hud;
     [SerializeField] private NewBehaviourScript habManager;
 
@@ -35,27 +34,19 @@ public class GameManager : MonoBehaviour
             return;
         }
 
-        // Suscribirse al evento de la carga de la escena
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // Asignar las referencias cuando la nueva escena se haya cargado
         if (hud == null)
-        {
             hud = FindObjectOfType<HUD>();
-        }
 
         if (habManager == null)
-        {
             habManager = FindObjectOfType<NewBehaviourScript>();
-        }
 
-        // Inicializar vidas y HUD si no es cero
         if (Vidas == 0)
         {
-            
             hud.ActivarVida(Vidas);
             Debug.Log("Tienes 3 vidas");
         }
@@ -71,12 +62,11 @@ public class GameManager : MonoBehaviour
         if (Vidas == 0)
         {
             PlayerController player = FindObjectOfType<PlayerController>();
-            if (player.respawnPoint != null)
+            if (player != null && player.respawnPoint != null)
             {
                 CheckpointData.ultimaPosicionCheckpoint = player.respawnPoint.position;
             }
 
-            // Recargar la escena
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
@@ -84,19 +74,15 @@ public class GameManager : MonoBehaviour
     public bool RecuperarVida()
     {
         if (Vidas == 3)
-        {
             return false;
-        }
 
-        hud.ActivarVida(Vidas);
         Vidas += 1;
-
+        hud.ActivarVida(Vidas);
         return true;
     }
 
     private void OnDestroy()
     {
-        // Desuscribirse del evento cuando el GameManager se destruya
         SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
